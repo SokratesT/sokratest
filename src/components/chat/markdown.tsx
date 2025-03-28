c6585c12e04d5756ad79fc06f7eba55b9e6123e7
LarsLrn <rotambus@googlemail.com>
2025-03-28 20:16:48 +0100

    Fixed TS compile error

--- src/components/chat/markdown.tsx
+++ src/components/chat/markdown.tsx
@@ -39,6 +39,7 @@ const components: Partial<Components> = {
   },
   a: ({ node, children, ...props }) => {
     return (
+      // @ts-expect-error
       <Link
         className="text-blue-500 hover:underline"
         target="_blank"
