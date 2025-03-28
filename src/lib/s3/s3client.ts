9656ede8bb632a5ada63af847fb0ed95188ae630
LarsLrn <rotambus@googlemail.com>
2025-03-28 13:34:58 +0100

    Fixed s3 default port

--- src/lib/s3/s3client.ts
+++ src/lib/s3/s3client.ts
@@ -2,7 +2,7 @@ import { Client } from "minio";
 
 export const s3Client = new Client({
   endPoint: process.env.S3_ENDPOINT || "",
-  port: Number(process.env.S3_PORT) || 9000,
+  port: Number(process.env.S3_PORT) || undefined,
   accessKey: process.env.S3_ACCESS_KEY,
   secretKey: process.env.S3_SECRET_KEY,
   useSSL: process.env.S3_USE_SSL === "true",
