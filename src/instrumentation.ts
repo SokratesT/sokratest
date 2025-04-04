29f5d679e74b5e32dbf035bd7a752502d4a16a46
LarsLrn <rotambus@googlemail.com>
2025-04-04 13:01:10 +0200

    Updated langfuse observability

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -5,8 +5,12 @@ import { posthog } from "posthog-js";
 
 export function register() {
   registerOTel({
-    serviceName: "langfuse-sokratest-next",
-    traceExporter: new LangfuseExporter(),
+    serviceName: "langfuse-sokratest-next-remote",
+    traceExporter: new LangfuseExporter({
+      secretKey: process.env.LANGFUSE_SECRET_KEY,
+      publicKey: process.env.LANGFUSE_PUBLIC_KEY,
+      baseUrl: process.env.LANGFUSE_BASEURL,
+    }),
   });
 }
 
