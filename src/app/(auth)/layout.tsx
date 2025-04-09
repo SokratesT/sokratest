152136d89fe4cc5054975f6ddb77e2a44020eee1
LarsLrn <rotambus@googlemail.com>
2025-04-09 13:44:11 +0200

    Refactored layout

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,7 @@
+import { FundingNotice } from "@/components/app/funding-notice";
 import { LogoAnimated } from "@/components/app/logo-animated";
 import { ThemeSwitcher } from "@/components/app/theme-switcher";
 import { AuroraBackground } from "@/components/ui/aceternity/aurora-background";
-import { buttonVariants } from "@/components/ui/button";
 import { ROUTES } from "@/settings/routes";
 import Link from "next/link";
 
@@ -11,38 +11,27 @@ const AuthLayout = async ({
   children: React.ReactNode;
 }>) => {
   return (
-    <AuroraBackground className="relative">
-      <ThemeSwitcher className="absolute top-4 right-4 text-foreground" />
-      <div className="z-10 flex h-screen flex-col items-center justify-center">
-        <div className="flex flex-col items-center justify-center gap-4">
+    <main className="flex min-h-screen flex-col">
+      <AuroraBackground className="-z-10 fixed inset-0 h-full w-full" />
+
+      <ThemeSwitcher className="fixed top-4 right-4 z-20 text-foreground" />
+
+      {/* Main content area that takes up available space */}
+      <div className="flex flex-grow items-center justify-center">
+        <div className="flex flex-col items-center justify-center gap-4 py-8">
           <Link href={ROUTES.PUBLIC.login.getPath()} className="cursor-default">
             <LogoAnimated variant="dynamic" />
           </Link>
 
           {children}
-
-          <div>
-            <p className="text-center text-muted-foreground text-sm">
-              SokratesT is currently in closed beta and not publicly available.
-            </p>
-            <div className="mt-4 flex justify-center gap-4 text-foreground">
-              <Link
-                href="https://www.hochschule-rhein-waal.de/de/fakultaeten/kommunikation-und-umwelt/forschungsprojekte/sokratest"
-                className={buttonVariants({ variant: "outline" })}
-              >
-                Learn more
-              </Link>
-              <Link
-                href="https://ki-edu-nrw.ruhr-uni-bochum.de/ueber-das-projekt/phase-2/praxis-transferprojekte/aktuelle-praxisprojekte/#sokratest"
-                className={buttonVariants({ variant: "outline" })}
-              >
-                KI:Edu.NRW
-              </Link>
-            </div>
-          </div>
         </div>
       </div>
-    </AuroraBackground>
+
+      {/* Funding notice at the bottom */}
+      <div className="my-4 flex w-full justify-around">
+        <FundingNotice />
+      </div>
+    </main>
   );
 };
 
