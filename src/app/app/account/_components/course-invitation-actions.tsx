5ef43a867c20ccb3441666b458e738e84890215e
LarsLrn <rotambus@googlemail.com>
2025-04-10 13:03:36 +0200

    Removed unused content

--- src/app/app/account/_components/course-invitation-actions.tsx
+++ src/app/app/account/_components/course-invitation-actions.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import {
   acceptCourseInvitation,
   rejectCourseInvitation,
@@ -34,16 +33,6 @@ export function CourseInvitationActions({
 
   return (
     <div className="mt-2 flex gap-2">
-      <Tabs defaultValue="account" className="w-[400px]">
-        <TabsList>
-          <TabsTrigger value="account">Account</TabsTrigger>
-          <TabsTrigger value="password">Password</TabsTrigger>
-        </TabsList>
-        <TabsContent value="account">
-          Make changes to your account here.
-        </TabsContent>
-        <TabsContent value="password">Change your password here.</TabsContent>
-      </Tabs>
       <Button onClick={handleAccept} variant="default" size="sm">
         Accept
       </Button>
