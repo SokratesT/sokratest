ce476dd4b09768395aeb4a7542e366ec91de576a
LarsLrn <rotambus@googlemail.com>
2025-04-10 15:24:28 +0200

    Removed quick actions

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -1,8 +1,8 @@
-import { QuickActions } from "@/components/app/quick-actions";
 import { UserStats } from "@/components/app/user-stats";
 import { UserWelcome } from "@/components/app/user-welcome";
 import { ChatsPreview } from "@/components/chat/chats-preview";
 import { CoursePreview } from "@/components/courses/course-preview";
+import { AppTourButton } from "@/components/next-step/app-tour-button";
 import { Skeleton } from "@/components/ui/skeleton";
 import type { Metadata } from "next";
 import { Suspense } from "react";
@@ -18,29 +18,22 @@ const AppPage = async () => {
         <UserWelcome />
       </Suspense>
 
+      <AppTourButton />
       <div className="mt-20 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4">
         <div className="flex flex-col gap-4 lg:col-span-3">
           <Suspense fallback={<Skeleton className="h-[86px] w-full" />}>
             <CoursePreview />
-          </Suspense>
-
-          <Suspense fallback={<Skeleton className="h-[86px] w-full" />}>
-            <ChatsPreview />
           </Suspense>
         </div>
         <div className="flex flex-col gap-4">
           <Suspense fallback={<Skeleton className="h-[86px] w-full" />}>
             <UserStats />
           </Suspense>
-
-          <div className="space-y-4">
-            <h2 className="font-semibold text-2xl tracking-tight">
-              Quick Actions
-            </h2>
-            <QuickActions />
-          </div>
         </div>
       </div>
+      <Suspense fallback={<Skeleton className="h-[86px] w-full" />}>
+        <ChatsPreview />
+      </Suspense>
     </div>
   );
 };
