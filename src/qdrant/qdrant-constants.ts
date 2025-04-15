84f254f0409a24c99ba5b07653fdf362629cae18
LarsLrn <rotambus@googlemail.com>
2025-04-15 12:43:21 +0200

    Added dev check

--- src/qdrant/qdrant-constants.ts
+++ src/qdrant/qdrant-constants.ts
@@ -1,6 +1,9 @@
 export const qdrantCollections = {
   chunks: {
-    name: "sokratest-chunks",
+    name:
+      process.env.NODE_ENV === "production"
+        ? "sokratest-chunks-PROD"
+        : "sokratest-chunks-DEV",
     dimensions: 4096,
     index: {
       courseId: "course_id",
