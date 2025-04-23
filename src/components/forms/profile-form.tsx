b50f4e117f00f732c72fb8a229a9e25ee4cc4470
LarsLrn <rotambus@googlemail.com>
2025-04-23 21:25:54 +0200

    Adjusted spacing

--- src/components/forms/profile-form.tsx
+++ src/components/forms/profile-form.tsx
@@ -56,7 +56,7 @@ const ProfileForm = () => {
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
         <FormField
           control={form.control}
           name="name"
