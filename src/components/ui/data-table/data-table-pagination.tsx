b48d46ca738545b6f8ad62eba49b4114b9248243
LarsLrn <rotambus@googlemail.com>
2025-04-24 14:08:13 +0200

    Adjusted page size dropdown width

--- src/components/ui/data-table/data-table-pagination.tsx
+++ src/components/ui/data-table/data-table-pagination.tsx
@@ -36,7 +36,7 @@ const DataTablePagination = () => {
               table.setPageSize(Number(value));
             }}
           >
-            <SelectTrigger className="h-8 w-[70px]">
+            <SelectTrigger className="h-8 w-[80px]">
               <SelectValue placeholder={table.getState().pagination.pageSize} />
             </SelectTrigger>
             <SelectContent side="top">
